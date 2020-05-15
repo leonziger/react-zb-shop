4707a776bf683a796c463ee4fe20a6339cfc7429
Uma <buldog52@gmail.com>
2020-05-15 15:58:10 +0300

    Updates

---
+++ precache-manifest.d27b0f5f71f42f2fcca5f1a51a9c9c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91016f1c810e8159de433c37f093b1bc",
+    "url": "/react-zb-shop/index.html"
+  },
+  {
+    "revision": "bef174db6eee600f5177",
+    "url": "/react-zb-shop/static/css/main.fe3e6cd9.chunk.css"
+  },
+  {
+    "revision": "4a9d6b8abaaf4af01b79",
+    "url": "/react-zb-shop/static/js/2.79f00de3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-zb-shop/static/js/2.79f00de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef174db6eee600f5177",
+    "url": "/react-zb-shop/static/js/main.09e47f92.chunk.js"
+  },
+  {
+    "revision": "bff347cb8382d99ba064",
+    "url": "/react-zb-shop/static/js/runtime-main.80e0d736.js"
+  }
+]);
